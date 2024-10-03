702edcd1a75ac6b4f520f0d58d91403b6b0171c7
project-wabot <wbotproject@gmail.com>
2024-10-03 15:08:23 +0530

    ❖ DARK NETHU MD ❖

--- plugins/main.js
+++ plugins/main.js
@@ -36,8 +36,6 @@ reply(`${e}`)
 })
 
 //============= menu command ================
-const config = require('../config')
-const {cmd , commands} = require('../command')
 const os = require("os")
 const {runtime} = require('../lib/functions')
 
