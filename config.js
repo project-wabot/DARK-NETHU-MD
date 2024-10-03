2c6b0a205dba8966dfc3c52fac5cdeb2bbbc4218
project-wabot <wbotproject@gmail.com>
2024-10-03 12:10:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID|| "put you session id",
-ALIVE_IMG : process.env.ALIVE_IMG || "",
+SESSION_ID: process.env.SESSION_ID|| "fOJHBZCT#MS3JGKhxP9v98YjD0tozNLInJf9wrhSY6NWF0byYHQI",
+ALIVE_IMG : process.env.ALIVE_IMG || "https://iili.io/dLpAgr7.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "*Hello I am alive now*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
