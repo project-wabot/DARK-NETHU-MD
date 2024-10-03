15c3ec942cfa21ed4794b57a3dc1f173f2aba337
project-wabot <wbotproject@gmail.com>
2024-10-03 11:37:55 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,18 +11,22 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let des = `*👋 Hello ${pushname}*
+let des = `*👋 Hello ${pushname} I'm alive now*
 
-*╔╭────────────╮╕*
-*╭│I'm Alive Now👾  │─◎◎▷*
-*╘╰────────────╯╜*
-*│A QUEEN-KYLIE-MD │Whatsapp Bot Based │Many │Services With A │RealTime* *Automated │Consversational* *││Experience, Enjoy💫.*
-*| So,I Think This Bots Are Useful To You.📍*
-*│*
-*│Type .menu To Get Bot User*
-*│Menu💫*
-*╰───────────────◎◎▷*
-*©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*`
+*╭─「 ALIVE 」
+*│◈ 𝚁𝚄𝙽𝚃𝙸𝙼𝙴 -* ${runtime(process.uptime())}
+*│◈ 𝚁𝙰𝙼 𝚄𝚂𝙰𝙶𝙴 -* 66.24MB / 126777MB
+*╰──────────●●►*
+*╭──────────●●►*
+*│ 𝚄𝙿𝙳𝙰𝚃𝙴 𝙲𝙷𝙰𝙽𝙿𝙾𝙽𝙴𝙻:* https://whatsapp.com/channel/0029VagCogPGufJ3kZWjsW3A
+*│
+*│ 𝙶𝙸𝚃𝙷𝚄𝙱:* https://github.com/project-wabot/DARK-NETHU-MD
+*╰──────────●●►*
+
+*Tipe .menu to get bot user menu*
+
+> ᴘᴏᴠᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴍɪᴋᴀ ᴍᴀɪɴ
+`
 return await conn.sendMessage(from,{image: {url: `https://files.catbox.moe/48p60j.jpg`},caption: des},{quoted: mek})
 }catch(e){
 console.log(e)
