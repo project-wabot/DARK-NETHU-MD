e9e043327bab8af35a342d7129db96db069fc711
©ʍɾ ℂ𝕪𝕓𝕖𝕣 ʍɑղմӀ օƒƒíϲíɑӀ♠️ <157870475+manulofficial7@users.noreply.github.com>
2024-10-03 20:38:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID|| "fOJHBZCT#MS3JGKhxP9v98YjD0tozNLInJf9wrhSY6NWF0byYHQI",
-ALIVE_IMG : process.env.ALIVE_IMG || "https://iili.io/dLpAgr7.jpg",
-ALIVE_MSG : process.env.ALIVE_MSG || "*Hello I am alive now*",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MONGODB: process.env.MONGODB|| "",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 AUTO_BIO:"true",
 AUTO_VOICE:"true"
