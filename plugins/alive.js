e2861036ce77b75c6856f3f832517066a64b8e92
project-wabot <wbotproject@gmail.com>
2024-10-03 11:02:48 +0530

    ❖ DARK NETHU MD ❖

---
+++ plugins/alive.js
@@ -0,0 +1,31 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "alive",
+    desc: "Check bot online or no.",
+    category: "main",
+    react: "👋",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let des = `*👋 Hello ${pushname}*
+
+*╔╭────────────╮╕*
+*╭│I'm Alive Now👾  │─◎◎▷*
+*╘╰────────────╯╜*
+*│A QUEEN-KYLIE-MD │Whatsapp Bot Based │Many │Services With A │RealTime* *Automated │Consversational* *││Experience, Enjoy💫.*
+*| So,I Think This Bots Are Useful To You.📍*
+*│*
+*│Type .menu To Get Bot User*
+*│Menu💫*
+*╰───────────────◎◎▷*
+*©Qᴜᴇᴇɴ ᴋʏʟɪᴇ-ᴍᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜッ*`
+return await conn.sendMessage(from,{image: {url: `https://files.catbox.moe/48p60j.jpg`},caption: des},{quoted: mek})
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
