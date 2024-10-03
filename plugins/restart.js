7ea8925ba2475ccb1fd3e4546626d09691795700
project-wabot <wbotproject@gmail.com>
2024-10-03 13:39:54 +0530

    ❖ DARK NETHU MD ❖

--- plugins/restart.js
+++ plugins/restart.js
@@ -13,7 +13,7 @@ async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender
 try{
 if(!isOwner) return
 const {exec} = require("child_process")
-reply("restarting...")
+reply("*Restarting...*")
 await sleep(1500)
 exec("pm2 restart all")
 }catch(e){
