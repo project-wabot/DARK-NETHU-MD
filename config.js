799330f8dadcd0cfa77c1144bae1db696339e16f
project-wabot <wbotproject@gmail.com>
2024-10-04 17:02:04 +0530

    ❖ DARK NETHU MD ❖

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID|| "kfUX2RgD#stx4rRdKEEedQSYAY71Iay4cc3nJ3gKqwy9Ndzxuyj0",
-MONGODB: process.env.MONGODB|| "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
+SESSION_ID: process.env.SESSION_ID || "kfUX2RgD#stx4rRdKEEedQSYAY71Iay4cc3nJ3gKqwy9Ndzxuyj0",
+MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/dbFAKoG.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 AUTO_BIO:"true",
