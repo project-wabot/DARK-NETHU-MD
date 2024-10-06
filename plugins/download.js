e91ed5f52caf1bedd196dfa9d0cf48dacfbeff23
project-wabot <wbotproject@gmail.com>
2024-10-06 15:50:49 +0530

    ❖ DARK NETHU MD ❖

--- plugins/download.js
+++ plugins/download.js
@@ -379,4 +379,45 @@ async (conn, mek, m, { from, quoted, body, q, reply }) => {
 });
 
 
-//============================ apk downloader ========================
+//============================ xnxx downloader ========================
+
+const xnxx = require("xnxx-dl");
+const premiumGroups = [];
+
+cmd({
+    pattern: "xnxx",
+    desc: "Downloads a video from XNXX",
+    use: ".xnxx <search_term>",
+    react: "📥",
+    category: "premium",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, q, reply }) => {
+
+    if (!premiumGroups.includes(from)) {
+        return reply("This command is available in premium groups only.");
+    }
+    const searchTerm = q.trim();
+    if (!searchTerm) return reply(`Please provide a search term`);
+
+    reply(`Searching For Your Video...`);
+    try {
+        const videoInfo = await xnxx.download(searchTerm);        
+        if (!videoInfo || !videoInfo.link_dl) {
+            return await conn.sendMessage(from, { react: { text: '❌', key: mek.key } });
+        }
+        reply(`Downloading video...`);
+        const videoUrl = videoInfo.link_dl;
+        await conn.sendMessage(
+            from,
+            { video : { url: videoUrl }, caption: '*ᴘᴀᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴍɪᴋᴀ ᴍᴀɪɴ*', mimetype: 'video/mp4' }, { quoted: mek }
+        );
+
+        await conn.sendMessage(from, { react: { text: '✅', key: mek.key } });
+
+    } catch (e) {
+        console.log(e);
+        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } });
+        reply(`Error: ${e.message}`);
+    }
+});
