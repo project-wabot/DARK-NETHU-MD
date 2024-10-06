5e83ba203c831fdc47f16b28132ec5396b5bd80a
project-wabot <wbotproject@gmail.com>
2024-10-06 17:56:02 +0530

    ❖ DARK NETHU MD ❖

--- config.js
+++ config.js
@@ -8,7 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "TIESmSBZ#5RKtbPLEFjfp--_UCPbnAsSAQcM26_pzIVqhV7UM4Zo",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/dbFAKoG.jpg",
-AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 AUTO_BIO:"true",
-AUTO_VOICE:"true"
 };
