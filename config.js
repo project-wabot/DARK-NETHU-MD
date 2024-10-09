bd39a21ceb793dd1eff4958fc8f90d7fd517e8fa
project-wabot <wbotproject@gmail.com>
2024-10-09 21:02:51 +0530

    ❖ DARK NETHU MD ❖

--- config.js
+++ config.js
@@ -5,8 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "uKoinIJR#dF4KuBLZz5p-BedwOYb_Z8t3Pe50uUwYU6YMrg-RxC0",
+SESSION_ID: process.env.SESSION_ID|| "kfUX2RgD#stx4rRdKEEedQSYAY71Iay4cc3nJ3gKqwy9Ndzxuyj0",
+MONGODB: process.env.MONGODB|| "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/dbFAKoG.jpg",
-
-BOT_NUMBER: process.env.BOT_NUMBER || "94704227534",
+AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
+AUTO_BIO:"true",
+AUTO_VOICE:"true"
 };
