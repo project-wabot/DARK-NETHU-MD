a66e0791a87fab29a85585f56136965eb214fdc6
project-wabot <wbotproject@gmail.com>
2024-10-09 20:53:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "TIESmSBZ#5RKtbPLEFjfp--_UCPbnAsSAQcM26_pzIVqhV7UM4Zo",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
+SESSION_ID: process.env.SESSION_ID || "uKoinIJR#dF4KuBLZz5p-BedwOYb_Z8t3Pe50uUwYU6YMrg-RxC0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/dbFAKoG.jpg",
 
 BOT_NUMBER: process.env.BOT_NUMBER || "94704227534",
